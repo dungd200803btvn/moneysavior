50c142219a2449ceeec3c194372cd74b8a6c6b81
Nguyen Duc Anh <nguyenducanh2105@gmail.com>
2025-05-09 18:56:36 +0900

    organize packages

--- src/main/java/soict/hedspi/itss2/gyatto/moneysavior/MoneySaviorApplication.java
+++ src/main/java/soict/hedspi/itss2/gyatto/moneysavior/MoneySaviorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoneysaviorApplication {
+public class MoneySaviorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoneysaviorApplication.class, args);
+		SpringApplication.run(MoneySaviorApplication.class, args);
 	}
 
 }
