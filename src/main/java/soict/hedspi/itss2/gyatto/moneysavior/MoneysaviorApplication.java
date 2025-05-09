90d809ed5c4df355e58cd06f0f2b55981c888461
Nguyen Duc Anh <nguyenducanh2105@gmail.com>
2025-05-09 18:49:21 +0900

    initial commit

---
+++ src/main/java/soict/hedspi/itss2/gyatto/moneysavior/MoneysaviorApplication.java
@@ -0,0 +1,13 @@
+package soict.hedspi.itss2.gyatto.moneysavior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneysaviorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneysaviorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soict/hedspi/itss2/gyatto/moneysavior/MoneysaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package soict.hedspi.itss2.gyatto.moneysavior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneysaviorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
